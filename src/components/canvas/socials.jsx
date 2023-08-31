c2586c2c15da48db3f5978f1c30d5e9b4a048b36
Lucas Torres <lucas772003@hotmail.com>
2023-08-31 00:09:32 -0300

    social-link-added

--- src/components/canvas/socials.jsx
+++ src/components/canvas/socials.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import "./social.css"
 
+
+
 function Social() {
   return (
-    <div className='social-text'>
-        <p>
-          Connect with me
-          <h2>Social Links:</h2>
-        </p>
+    <div>
+        <div className='social-text'>
+            <p>
+            Connect with me
+            <h2>Social Links:</h2>
+            </p>
+        </div>
+       <div className='animations'>
+            <div className='social-img'>
+                <a href="lucas7320030@hotmail.com"><img src="../images/mail.png" alt="mail" /></a>
+            </div>
+            <div className='social-img'>
+                <a href="https://www.linkedin.com/in/lucas-torres-a56a8027a/" target='blank'><img src="../images/linkedin-social.png" alt="linkedin" /></a>
+            </div>
+            <div className='social-img'>
+                <a href="https://github.com/CapiLucas" target='blank'><img src="../images/github.png" alt="github" /></a>
+            </div>
+       </div>
     </div>
+    
   )
 }
 
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import LogoSvg from "../../assets/logo.svg";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <div className="navbarDiv">
       <nav className="navbg">
         <div className="navleft">
-        <img src={LogoSvg} alt="logo" className="logo" />
+          <img src={LogoSvg} alt="logo" className="logo" />
           <Link
             to="/"
             onClick={() => {
@@ -16,16 +22,24 @@ function Navbar() {
             className="link-no-decoration"
           >
             <p className="name">
-              Lucas
-              <span>| Frontend Developer</span>
+              Lucas <span>| Frontend Developer</span>
             </p>
           </Link>
         </div>
-        <div className="navright">
+        <div className={`navright ${menuOpen ? "open" : ""}`}>
+          <div className="menu-icon" onClick={toggleMenu}>
+            <i class="fa-solid fa-bars"></i>         
+          </div>
           <ul>
-            <li><Link to={"./"}>Home</Link></li>
-            <li><Link to={"./work"}>Work</Link></li>
-            <li><Link to={"./contact"}>Contact</Link></li>
+            <li>
+              <Link to={"./"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"./work"}>Work</Link>
+            </li>
+            <li>
+              <Link to={"./contact"}>Contact</Link>
+            </li>
           </ul>
         </div>
       </nav>
