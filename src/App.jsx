d617e6302a00994e2491a57b4aaf5d7d898e055e
Lucas Torres <lucas772003@hotmail.com>
2023-09-10 23:38:50 -0300

    navbar script fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/header/Navbar';
 import Home from './components/pages/Home';
 import Experience from './components/pages/Experience';
+import Navbar from './components/header/navbar';
 
 function App() {
   return (
