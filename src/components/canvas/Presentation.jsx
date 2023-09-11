f735004ed6a0f57fd3a8f44cfc6bc74a46ca725c
Lucas Torres <lucas772003@hotmail.com>
2023-09-11 15:10:43 -0300

    typewritter effect

--- src/components/canvas/Presentation.jsx
+++ src/components/canvas/Presentation.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './presentation.css';
 
 function Presentation() {
+  const textToType = "  Frontend Developer take a look";
+  const [typedText, setTypedText] = useState('');
+  const charIndex = useRef(0);
+
+  useEffect(() => {
+    const typingTimer = setInterval(() => {
+      if (charIndex.current < textToType.length) {
+        setTypedText((prevText) => prevText + textToType.charAt(charIndex.current));
+        charIndex.current += 1;
+      } else {
+        clearInterval(typingTimer);
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(typingTimer);
+    };
+  }, []);
+
   return (
     <div className='canvaBG'>
-        <section className='canvaP'>
-            <div>
-                <h1 className='presentation'>
-                    Hi, I'm{" "}
-                    <span>
-                        Lucas Torres
-                    </span>
-                </h1>
-                <p>
-                    a frontend Developer <br></br>
-                    Take a look
-                </p>
-            </div>
-            <div>
-                <img src="../images/me.png" alt="me" />
-            </div>
-        </section>
+      <section className='canvaP'>
+        <div>
+          <h1 className='presentation'>
+            Hi, I'm{" "}
+            <span>
+              Lucas Torres
+            </span>
+          </h1>
+          <p>
+            a {typedText}
+          </p>
+        </div>
+        <div>
+          <img src="../images/me.png" alt="me" />
+        </div>
+      </section>
     </div>
   );
 }
